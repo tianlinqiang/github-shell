febcdbf048ec4075c5957b18e72b663da0699a8f
tianlinqiang <tlq666forjob@163.com>
2020-07-03 11:26:27 +0800

    add 7.3

--- 16-02-cp-bin-lib.sh
+++ 16-02-cp-bin-lib.sh
@@ -4,18 +4,20 @@
 
 copy_order(){
 
-  DIR=$destdir_order$tmp2_dir
+  DIR="$destdir_order$tmp2_dir/"
   if [ ! -d $DIR ];then
-    mkdir $DIR
+    mkdir $DIR #/mnt/sysroot/bin/
   fi
-  if [ -d $DIR ];then
-    if [ ! -e $DIR$basename ];then
-      cp $path_order $DIR
-      echo "$DIR$basename copy success."
-    else 
-      echo "$DIR$basename exist."
-    fi
+  if [ ! -e $DIR$basename ];then #/mnt/sysroot/bin/ls
+    cp $path_order $DIR
+    echo "$DIR$basename copy success."
+    echo "#############################"
+  else 
+    echo "$DIR$basename exist."
+    echo "#############################"
+    exit 1
   fi
+  
 }
 
 
@@ -26,16 +28,16 @@ get_lib_array(){
 
 cp_lib(){
  
-  destdir_lib=${destdir_order%/*}
+  destdir_lib=${destdir_order%/*}   #/mnt/sysroot
   get_lib_array
   for i in $(seq 0 $((${#ldds_array[*]}-1)));do
-      tmp_dir=$destdir_lib${ldds_array[$i]}
-      mkdir_dir=${tmp_dir%/*}
+      tmp_dir=$destdir_lib${ldds_array[$i]}  #/mnt/sysroot/lib/.../xx.io
+      mkdir_dir=${tmp_dir%/*}  #/mnt/sysroot/lib/...
     if [ ! -d $mkdir_dir ];then
       mkdir -p $mkdir_dir
-    else
-      if [ ! -e $destdir_lib${ldds_array[$i]} ];then
-      if `cp -f ${ldds_array[$i]} $destdir_lib${ldds_array[$i]}`;then
+    fi
+      if [ ! -e $destdir_lib${ldds_array[$i]} ];then #/mnt/sysroot/lib/./xx.io
+      if `cp -f ${ldds_array[$i]} $mkdir_dir`;then
         echo "$destdir_lib${ldds_array[$i]} copy success."
       else
         echo "copy $destdir_lib${ldds_array[$i]} fail."
@@ -43,17 +45,32 @@ cp_lib(){
       else
         echo "$destdir_lib${ldds_array[$i]} exist."
       fi
-    fi
   done
 }
 
-while true;do
-  read -p "pl input a order:" order
-  destdir_order=/mnt/sysroot/
+main(){
+
+  destdir_order=/mnt/sysroot/  #new /  : /mnt/sysroot/
   path_order=$(which $order)
-  basename="$(basename $path_order)"
-  tmp1_dir=${path_order%/*}
-  tmp2_dir=${tmp1_dir#*/}
-  copy_order
-  cp_lib
+  if (($?==0));then #order path
+    basename="$(basename $path_order)" #order
+    tmp1_dir=${path_order%/*} #/bin
+    tmp2_dir=${tmp1_dir#*/}   #bin
+    copy_order
+    cp_lib
+  else
+    echo "no $order order."
+    exit 1
+ fi
+}
+while true;do
+  read -p "pl input a Order or quit:" order
+  case $order in
+  quit)
+    exit 0
+    ;;
+  *)
+    main
+    ;;
+esac
 done
